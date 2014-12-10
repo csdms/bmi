ca0920999233f818db3748f5d95d989d8715d779
mcflugen <mcflugen@gmail.com>
2014-12-10 16:07:37 -0700

    Added BMI_POISSON namespace to prototypes.

--- bindings/c/bmi/bmi_poisson.h
+++ bindings/c/bmi/bmi_poisson.h
@@ -5,61 +5,57 @@
 extern "C" {
 #endif
 
-#define BMI_POISSON_INPUT_VAR_NAME_COUNT 1
-#define BMI_POISSON_OUTPUT_VAR_NAME_COUNT 1
-
-typedef struct _BMI_Model BMI_Model;
 
 /* Model Control functions */
-int BMI_POISSON_Initialize (const char *, BMI_Model**);
-int BMI_POISSON_Update (BMI_Model *);
-int BMI_POISSON_Update_until (BMI_Model *, double);
-int BMI_POISSON_Finalize (BMI_Model *);
-int BMI_POISSON_Run_model (BMI_Model *);
+int BMI_POISSON_Initialize (const char *, void**);
+int BMI_POISSON_Update (void *);
+int BMI_POISSON_Update_until (void *, double);
+int BMI_POISSON_Finalize (void *);
+int BMI_POISSON_Run_model (void *);
 
 /* Model information functions */
-int BMI_POISSON_Get_component_name (BMI_Model *, char *);
-int BMI_POISSON_Get_input_var_name_count (BMI_Model, int *);
-int BMI_POISSON_Get_output_var_name_count (BMI_Model, int *);
-int BMI_POISSON_Get_input_var_names (BMI_Model *, char **);
-int BMI_POISSON_Get_output_var_names (BMI_Model *, char **);
+int BMI_POISSON_Get_component_name (void *, char *);
+int BMI_POISSON_Get_input_var_name_count (void *, int *);
+int BMI_POISSON_Get_output_var_name_count (void *, int *);
+int BMI_POISSON_Get_input_var_names (void *, char **);
+int BMI_POISSON_Get_output_var_names (void *, char **);
 
 /* Variable information functions */
-int BMI_POISSON_Get_var_type (BMI_Model *, const char *, BMI_Var_type *);
-int BMI_POISSON_Get_var_units (BMI_Model *, const char *, char *);
-int BMI_POISSON_Get_var_rank (BMI_Model *, const char *, int *);
-int BMI_POISSON_Get_current_time (BMI_Model *, double *);
-int BMI_POISSON_Get_start_time (BMI_Model *, double *);
-int BMI_POISSON_Get_end_time (BMI_Model *, double *);
-int BMI_POISSON_Get_time_units (BMI_Model *, char *);
-int BMI_POISSON_Get_time_step (BMI_Model *, double *);
+int BMI_POISSON_Get_var_type (void *, const char *, BMI_Var_type *);
+int BMI_POISSON_Get_var_units (void *, const char *, char *);
+int BMI_POISSON_Get_var_rank (void *, const char *, int *);
+int BMI_POISSON_Get_current_time (void *, double *);
+int BMI_POISSON_Get_start_time (void *, double *);
+int BMI_POISSON_Get_end_time (void *, double *);
+int BMI_POISSON_Get_time_units (void *, char *);
+int BMI_POISSON_Get_time_step (void *, double *);
 
 /* Variable getter and setter functions */
-int BMI_POISSON_Get_value (BMI_Model *, const char *, void *);
-int BMI_POISSON_Get_value_ptr (BMI_Model *, const char *, void **);
-int BMI_POISSON_Get_value_at_indices (BMI_Model *, const char *, void *, int *,
+int BMI_POISSON_Get_value (void *, const char *, void *);
+int BMI_POISSON_Get_value_ptr (void *, const char *, void **);
+int BMI_POISSON_Get_value_at_indices (void *, const char *, void *, int *,
     int);
 
-int BMI_POISSON_Set_value (BMI_Model *, const char *, void *);
-int BMI_POISSON_Set_value_ptr (BMI_Model *, const char *, void **);
-int BMI_POISSON_Set_value_at_indices (BMI_Model *, const char *, int *, int,
+int BMI_POISSON_Set_value (void *, const char *, void *);
+int BMI_POISSON_Set_value_ptr (void *, const char *, void **);
+int BMI_POISSON_Set_value_at_indices (void *, const char *, int *, int,
     void *);
 
 /* Grid information functions */
-int BMI_POISSON_Get_grid_shape (BMI_Model *, const char *, int *);
-int BMI_POISSON_Get_grid_spacing (BMI_Model *, const char *, double *);
-int BMI_POISSON_Get_grid_origin (BMI_Model *, const char *, double *);
+int BMI_POISSON_Get_grid_shape (void *, const char *, int *);
+int BMI_POISSON_Get_grid_spacing (void *, const char *, double *);
+int BMI_POISSON_Get_grid_origin (void *, const char *, double *);
 
-int BMI_POISSON_Get_grid_x (BMI_Model *, const char *, double *);
-int BMI_POISSON_Get_grid_y (BMI_Model *, const char *, double *);
-int BMI_POISSON_Get_grid_z (BMI_Model *, const char *, double *);
+int BMI_POISSON_Get_grid_x (void *, const char *, double *);
+int BMI_POISSON_Get_grid_y (void *, const char *, double *);
+int BMI_POISSON_Get_grid_z (void *, const char *, double *);
 
-int BMI_POISSON_Get_grid_cell_count (BMI_Model *, const char *, int *);
-int BMI_POISSON_Get_grid_point_count (BMI_Model *, const char *, int *);
-int BMI_POISSON_Get_grid_vertex_count (BMI_Model *, const char *, int *);
+int BMI_POISSON_Get_grid_cell_count (void *, const char *, int *);
+int BMI_POISSON_Get_grid_point_count (void *, const char *, int *);
+int BMI_POISSON_Get_grid_vertex_count (void *, const char *, int *);
 
-int BMI_POISSON_Get_grid_connectivity (BMI_Model *, const char *, int *);
-int BMI_POISSON_Get_grid_offset (BMI_Model *, const char *, int *);
+int BMI_POISSON_Get_grid_connectivity (void *, const char *, int *);
+int BMI_POISSON_Get_grid_offset (void *, const char *, int *);
 
 #if defined(__cplusplus)
 }
