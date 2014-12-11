1eb1bbc42f4102ba9f92e7376f436945516f2d65
mcflugen <mcflugen@gmail.com>
2014-12-10 18:02:52 -0700

    Loop over time and print some output.

--- bindings/c/poisson/main.c
+++ bindings/c/poisson/main.c
@@ -1,11 +1,25 @@
+#include <stdio.h>
+
 #include "poisson.h"
   
 int
 main(void)
 {
   PoissonModel * p = poisson_from_default ();
+  int i, j, t;
 
-  poisson_advance_in_time (p);
+  for (t=0; t<10; t++) {
+    poisson_advance_in_time (p);
+
+    fprintf (stdout, "Time: %f\n", p->t);
+
+    for (i=0; i<p->shape[0]; i++) {
+      for (j=0; j<p->shape[1]; j++)
+        fprintf (stdout, "%f ", p->z[i][j]);
+      fprintf (stdout, "\n");
+    }
+    fprintf (stdout, "\n");
+  }
 
   return 0;
 }
