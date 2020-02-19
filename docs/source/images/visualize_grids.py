0139495a8920c77db044524b1f018bb4a9069b20
Mark Piper <mark.piper@colorado.edu>
2020-02-19 10:32:46 -0700

    Adjust colors and fonts for text labels

--- docs/source/images/visualize_grids.py
+++ docs/source/images/visualize_grids.py
@@ -41,9 +41,9 @@ ax.add_patch(patch)
 vx, vy = zip(*path.vertices)
 line, = ax.plot(vx, vy, 'go-')
 
-plt.text(4.1, 1.75, 'nodes', color='r', fontsize=12, va='center')
-plt.text(4.1, 1.5, 'edges', color='g', fontsize=12, va='center')
-plt.text(4.1, 1.25, 'faces', color='b', fontsize=12, va='center')
+plt.text(4.15, 1.8, 'nodes', color='r', fontsize=12, va='center')
+plt.text(4.15, 1.5, 'edges', color='b', style='italic', fontsize=12, va='center')
+plt.text(4.15, 1.2, 'faces', weight='bold', fontsize=12, va='center')
 
 # Nodes
 for i in range(len(x)-2):
@@ -52,19 +52,19 @@ plt.text(x[4]+0.1, y[4], '4', color='r', fontsize=12)
 plt.text(x[5], y[5]+0.1, '5', color='r', fontsize=12)
 
 # Edges
-plt.text(0.5*(x[0]+x[1])+0.05, 0.5*(y[0]+y[1]), '0', color='g', fontsize=12)
-plt.text(0.5*(x[1]+x[2])-0.15, 0.5*(y[1]+y[2]), '1', color='g', fontsize=12)
-plt.text(0.5*(x[2]+x[3])+0.05, 0.5*(y[2]+y[3]), '2', color='g', fontsize=12)
-plt.text(0.5*(x[3]+x[0]), 0.5*(y[3]+y[0])-0.15, '3', color='g', fontsize=12)
-plt.text(0.5*(x[1]+x[4]), 0.5*(y[1]+y[4])+0.05, '4', color='g', fontsize=12)
-plt.text(0.5*(x[4]+x[5])-0.15, 0.5*(y[4]+y[5]), '5', color='g', fontsize=12)
-plt.text(0.5*(x[5]+x[2]), 0.5*(y[5]+y[2])-0.15, '6', color='g', fontsize=12)
-plt.text(0.5*(x[5]+x[3]), 0.5*(y[5]+y[3])+0.05, '7', color='g', fontsize=12)
+plt.text(0.5*(x[0]+x[1])+0.05, 0.5*(y[0]+y[1]), '0', color='b', style='italic', fontsize=12)
+plt.text(0.5*(x[1]+x[2])-0.15, 0.5*(y[1]+y[2]), '1', color='b', style='italic', fontsize=12)
+plt.text(0.5*(x[2]+x[3])+0.05, 0.5*(y[2]+y[3]), '2', color='b', style='italic', fontsize=12)
+plt.text(0.5*(x[3]+x[0]), 0.5*(y[3]+y[0])-0.15, '3', color='b', style='italic', fontsize=12)
+plt.text(0.5*(x[1]+x[4]), 0.5*(y[1]+y[4])+0.05, '4', color='b', style='italic', fontsize=12)
+plt.text(0.5*(x[4]+x[5])-0.15, 0.5*(y[4]+y[5]), '5', color='b', style='italic', fontsize=12)
+plt.text(0.5*(x[5]+x[2]), 0.5*(y[5]+y[2])-0.15, '6', color='b', style='italic', fontsize=12)
+plt.text(0.5*(x[5]+x[3]), 0.5*(y[5]+y[3])+0.05, '7', color='b', style='italic', fontsize=12)
 
 # Faces
-plt.text(0.95, 2.5, '0', color='b', fontsize=12)
-plt.text(2.5, 1.3, '1', color='b', fontsize=12)
-plt.text(2.3, 2.9, '2', color='b', fontsize=12)
+plt.text(0.9, 2.5, '0', weight='bold', fontsize=12)
+plt.text(2.5, 1.3, '1', weight='bold', fontsize=12)
+plt.text(2.3, 2.9, '2', weight='bold', fontsize=12)
 
 ax.grid(alpha=0.5)
 ax.axis('equal')
