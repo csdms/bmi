e7a5b5c76b67b3b0819066f0636954eac5b961d8
mcflugen <mcflugen@gmail.com>
2024-11-09 07:15:32 -0700

    remove templates path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,9 +34,6 @@ extensions = [
     "sphinx_design",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 # source_suffix = [".rst", ".md"]
