d8a8c915068dbd3bd8b21d1d50dc7889deb83341
mcflugen <mcflugen@gmail.com>
2024-11-09 07:06:13 -0700

    add a light and dark style for syntax highlighting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,6 +96,7 @@ exclude_patterns = ["links.md"]
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = "sphinx"
+pygments_dark_style = "monokai"
 
 # A list of ignored prefixes for module index sorting.
 # modindex_common_prefix = []
